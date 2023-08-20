65d1298efcb30b5b5ea47f574ca093434cc87642
r3morce <mathias.schmidt@hey.com>
2023-08-20 21:16:35 +0200

    Read .env

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import os
 
 from dotenv import load_dotenv
 
+from modules.output import print_hello
+
 load_dotenv()
 
-
-from modules.output import print_hello
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
@@ -30,7 +30,12 @@ if DEBUG:
 
 def main():
     logging.info("Script started")
+
     print_hello()
+
+    logging.debug("API_KEY from .env")
+    logging.debug(os.environ.get("API_KEY"))
+
     logging.info("Script ended")
 
 
